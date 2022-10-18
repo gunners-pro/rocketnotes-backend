b713fcdafa3d8974f18918e96f6d3d684170a5ca
Fabricyo Barreto <bass.barreto@gmail.com>
2022-10-17 22:11:11 -0300

    chore: config migration createUser

---
+++ src/database/migrations/20221018010107_createUsers.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', table => {
+    table.increments().notNullable();
+    table.text('name').notNullable();
+    table.text('email').notNullable();
+    table.text('password').notNullable();
+    table.text('avatar').nullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users');
+}
