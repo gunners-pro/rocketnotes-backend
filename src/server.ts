f0696ee731f8b0e23d44726dc274433f976245e9
Fabricyo Barreto <bass.barreto@gmail.com>
2022-10-25 19:57:59 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import 'express-async-errors';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 
 import { upload_folder } from './config/upload';
@@ -6,7 +7,7 @@ import { routes } from './routes';
 import { AppError } from './utils/AppError';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(upload_folder));
